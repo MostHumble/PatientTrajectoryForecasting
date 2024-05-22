800676d314beb4fef24459b0e2d86f56db0b719f
sifal.klioui <sifal.klioui@sms-cluster.lis-lab.fr>
2024-05-22 21:07:25 +0200

    fix str

--- utils/notes.py
+++ utils/notes.py
@@ -19,30 +19,30 @@ class TextPreprocessor:
 
     def __call__(self, note: str) -> str:
         if self.lower:
-            note = note.str.lower()
+            note = note.lower()
         if self.convert_danish_characters:
-            note = note.str.replace("å", "aa", regex=True)
-            note = note.str.replace("æ", "ae", regex=True)
-            note = note.str.replace("ø", "oe", regex=True)
+            note = note.replace("å", "aa", regex=True)
+            note = note.replace("æ", "ae", regex=True)
+            note = note.replace("ø", "oe", regex=True)
         if self.remove_accents:
-            note = note.str.replace("é|è|ê", "e", regex=True)
-            note = note.str.replace("á|à|â", "a", regex=True)
-            note = note.str.replace("ô|ó|ò", "o", regex=True)
+            note = note.replace("é|è|ê", "e", regex=True)
+            note = note.replace("á|à|â", "a", regex=True)
+            note = note.replace("ô|ó|ò", "o", regex=True)
         if self.remove_brackets:
-            note = note.str.replace("\[[^]]*\]", "", regex=True)
+            note = note.replace("\[[^]]*\]", "", regex=True)
         if self.remove_special_characters:
-            note = note.str.replace("\n|/|-", " ", regex=True)
-            note = note.str.replace(
+            note = note.replace("\n|/|-", " ", regex=True)
+            note = note.replace(
                 "[^a-zA-Z0-9 ]", "", regex=True
             )
         if self.remove_special_characters_mullenbach:
-            note = note.str.replace(
+            note = note.replace(
                 "[^A-Za-z0-9]+", " ", regex=True
             )
         if self.remove_digits:
-            note = note.str.replace("(\s\d+)+\s", " ", regex=True)
+            note = note.replace("(\s\d+)+\s", " ", regex=True)
 
-        note = note.str.replace("\s+", " ", regex=True)
-        note = note.str.strip()
+        note = note.replace("\s+", " ", regex=True)
+        note = note.strip()
         return note
     
