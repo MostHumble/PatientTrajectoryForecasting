3d81acadcec4f924f73d59601606fecb738f6c3a
sifal.klioui <sifal.klioui@sms-cluster.lis-lab.fr>
2024-06-08 15:04:21 +0200

    add hostpitals ids store and low
    this maintains backward compatibility

--- utils/utils.py
+++ utils/utils.py
@@ -3,7 +3,7 @@ import pickle
 from typing import Dict, Tuple, List, Optional
 
 def load_data(path: str = 'outputData/originalData/' , updated_ids_to_types : bool = False,
-               train : bool = False, processed_data : bool = False) -> Tuple[List[List[List[int]]], Dict[str, int], Dict[str, int], Dict[int, str]]:
+               train : bool = False, processed_data : bool = False, with_notes : bool = False) -> Tuple[List[List[List[int]]], Dict[str, int], Dict[str, int], Dict[int, str]]:
         """
         Load data from the specified file.
 
@@ -17,17 +17,23 @@ def load_data(path: str = 'outputData/originalData/' , updated_ids_to_types : bo
             - The third element is a dictionary mapping event codes to their corresponding types.
             - The fourth element is a dictionary mapping event codes to their corresponding types (reversed mapping).
         """
+
         if train:
 
             source_target_sequences = pickle.load(open(os.path.join(path, 'source_target_sequences.pkl'), 'rb'))
             ids_to_types_map = pickle.load(open(os.path.join(path, 'ids_to_types.pkl'), 'rb'))
             tokens_to_ids_map = pickle.load(open(os.path.join(path, 'tokens_to_ids.pkl'), 'rb'))
             ids_to_tokens_map = pickle.load(open(os.path.join(path, 'ids_to_tokens.pkl'), 'rb'))
+            if with_notes:
+                subject_id_hadm_map = pickle.load(open(os.path.join(path, 'subject_id_hadm_map.pkl'), 'rb'))
             if updated_ids_to_types:
                 updated_ids_to_types =  pickle.load(open(os.path.join(path, 'updated_ids_to_types.pkl'), 'rb'))
                 reverse_up_ids_to_types =  pickle.load(open(os.path.join(path, 'reverse_up_ids_to_types.pkl'), 'rb'))
+                if with_notes:
+                    return source_target_sequences, ids_to_types_map, tokens_to_ids_map, updated_ids_to_types, reverse_up_ids_to_types, subject_id_hadm_map
                 return source_target_sequences, ids_to_types_map, tokens_to_ids_map, updated_ids_to_types, reverse_up_ids_to_types
-            
+            if with_notes:
+                return source_target_sequences, ids_to_types_map, tokens_to_ids_map, ids_to_tokens_map, subject_id_hadm_map
             return source_target_sequences, ids_to_types_map, tokens_to_ids_map, ids_to_tokens_map
         
         if processed_data:
@@ -54,21 +60,29 @@ def load_data(path: str = 'outputData/originalData/' , updated_ids_to_types : bo
             try:
                 target_tokens_to_ids = pickle.load(open(os.path.join(path, 'target_tokens_to_ids.pkl'), 'rb'))
             except:
-                target_tokens_to_ids = None    
+                target_tokens_to_ids = None
+            try:
+                hospital_ids_source = pickle.load(open(os.path.join(path, 'hospital_ids_source.pkl'), 'rb'))
+            except:
+                hospital_ids_source = None    
 
-            return source_sequences, target_sequences, source_tokens_to_ids, target_tokens_to_ids, old_to_new_ids_source, old_to_new_ids_target
+            return source_sequences, target_sequences, source_tokens_to_ids, target_tokens_to_ids, old_to_new_ids_source, old_to_new_ids_target, hospital_ids_source
 
         else:
         
             patients_visits_sequences = pickle.load(open(os.path.join(path, 'patients_visits_sequences.pkl'), 'rb'))
             tokens_to_ids_map = pickle.load(open(os.path.join(path, 'tokens_to_ids.pkl'), 'rb')) 
             ids_to_types_map = pickle.load(open(os.path.join(path, 'ids_to_types.pkl'), 'rb'))
+            try:
+                subject_id_hadm_id_map_notes = pickle.load(open(os.path.join(path, 'subject_id_hadm_map.pkl'), 'rb'))
+            except:
+                subject_id_hadm_id_map_notes = None
 
-            return patients_visits_sequences, tokens_to_ids_map, ids_to_types_map
+            return patients_visits_sequences, tokens_to_ids_map, ids_to_types_map, subject_id_hadm_id_map_notes
         
 def get_paths(config: dict, strategy : str = None, predict_procedure : bool = False, predict_drugs : bool = False,
                train : bool = False, processed_data = False,
-                 prepared_note_file : bool = True) -> dict:
+                 with_notes : bool = False) -> dict:
     """
     creates relevant paths according the given parameters
 
@@ -91,22 +105,25 @@ def get_paths(config: dict, strategy : str = None, predict_procedure : bool = Fa
              raise ValueError('Wrong strategy, must choose either TF, SDP')
 
     output_files =  config['output_files']
-    
+    if with_notes:
+        suffix = '_with_notes/'
+    else:
+        suffix = '/'
     paths = {}
     output_file = config['output_path']
     root = config['root_path']
     mimic_iv_path = os.path.join(root, config['mimic_iv_path'])
     ccs_file_path = os.path.join(root, config['ccs_path'])
-
+  
     if train:
         if predict_procedure and predict_drugs :
-            paths['train_data_path'] = os.path.join(root, output_file, strategy, output_files['all_outputs_file'])
+            paths['train_data_path'] = os.path.join(root, output_file, strategy, output_files['all_outputs_file'][:-1] + suffix)
         if predict_procedure and not(predict_drugs):
-            paths['train_data_path'] = os.path.join(root, output_file, strategy, output_files['diagnosis_procedure_output_file'])
+            paths['train_data_path'] = os.path.join(root, output_file, strategy, output_files['diagnosis_procedure_output_file'][:-1] + suffix)
         else:
-            paths['train_data_path'] = os.path.join(root, output_file, strategy, output_files['diagnosis_output_file'])
+            paths['train_data_path'] = os.path.join(root, output_file, strategy, output_files['diagnosis_output_file'][:-1] + suffix)
         if processed_data:
-            paths['processed_data_path'] = os.path.join(root, paths['train_data_path'], 'processed_data/')
+            paths['processed_data_path'] = os.path.join(root, paths['train_data_path'], 'processed_data' + suffix)
         return paths
         
     for mimic_file, path in config['mimic_files'].items():
@@ -114,8 +131,8 @@ def get_paths(config: dict, strategy : str = None, predict_procedure : bool = Fa
 
     for ccs_file, path in config['css_files'].items():
         paths[ccs_file] = os.path.join(ccs_file_path, path) 
-    if not prepared_note_file:
-        paths['note_file'] = os.path.join(root, config['mimic_iv_notes_path'])
+        
+    paths['note_file'] = os.path.join(root, config['mimic_iv_notes_path'])
     return paths
     
 
@@ -135,6 +152,8 @@ def store_files(source_target_sequences : List[Tuple[List[int], List[int]]] = No
                 old_to_new_ids_target = None,
                 source_sequences = None,
                 target_sequences = None,
+                subject_id_hadm_map = None,
+                hospital_ids_source = None,
                 output_file : str = 'outputData/originalData/',
                 **kw):
     """
@@ -158,6 +177,9 @@ def store_files(source_target_sequences : List[Tuple[List[int], List[int]]] = No
             pickle.dump(tokens_to_ids_map, open(os.path.join(output_file, 'tokens_to_ids.pkl'), 'wb'), -1)
         if ids_to_tokens_map is not None:
             pickle.dump(ids_to_tokens_map, open(os.path.join(output_file, 'ids_to_tokens.pkl'), 'wb'), -1)
+        if subject_id_hadm_map is not None:
+                pickle.dump(subject_id_hadm_map, open(os.path.join(output_file, 'subject_id_hadm_map.pkl'), 'wb'), -1)
+
         return
     if processed_data:
         if source_sequences is not None:
@@ -172,6 +194,8 @@ def store_files(source_target_sequences : List[Tuple[List[int], List[int]]] = No
             pickle.dump(source_tokens_to_ids, open(output_file + 'source_tokens_to_ids.pkl', 'wb'), -1)
         if target_tokens_to_ids is not None:
             pickle.dump(target_tokens_to_ids, open(output_file + 'target_tokens_to_ids.pkl', 'wb'), -1)
+        if hospital_ids_source is not None:
+            pickle.dump(hospital_ids_source, open(output_file + 'hospital_ids_source.pkl', 'wb'), -1)
         
         return
     else:
