015117caec2b33a9c467d498a52336bf41825573
sifal.klioui <sifal.klioui@sms-cluster.lis-lab.fr>
2024-06-11 15:29:12 +0200

    better api for num_embedding_layers

--- utils/bert_embeddings.py
+++ utils/bert_embeddings.py
@@ -15,7 +15,9 @@ class MosaicBertForEmbeddingGeneration(BertPreTrainedModel):
     """
 
     def __init__(self, config):
-        super().__init__(config)
+
+        assert config.num_hidden_layers >= config.num_embedding_layers, 'num_hidden_layers should be greater than or equal to num_embedding_layers'
+        
         self.config = config
         self.bert = BertModel(config)
        # this resets the weights
@@ -65,10 +67,9 @@ class MosaicBertForEmbeddingGeneration(BertPreTrainedModel):
         return_dict: Optional[bool] = None,
         subset_mask : Optional[torch.Tensor] = None,
         masked_tokens_mask: Optional[torch.Tensor] = None,
-        num_layers: Optional[int] = 4,
         hospital_ids_lens: list = None,
     ) -> torch.Tensor:
-
+        
         return_dict = return_dict if return_dict is not None else self.config.use_return_dict
 
         if masked_tokens_mask is None:
@@ -83,9 +84,9 @@ class MosaicBertForEmbeddingGeneration(BertPreTrainedModel):
             output_all_encoded_layers=True,
             subset_mask=subset_mask)
         
-        return self.get_embeddings(encoder_outputs_all, hospital_ids_lens, num_layers)
+        return self.get_embeddings(encoder_outputs_all, hospital_ids_lens, self.config.num_embedding_layers)
      
-    def get_embeddings(self, encoder_outputs_all, hospital_ids_lens, num_layers=4):
+    def get_embeddings(self, encoder_outputs_all, hospital_ids_lens, num_layers):
         # num_layer (we use 4), batch_size (concatenated visits), seq_len (clinical note sequences), hidden_dim.
         sentence_representation = torch.stack(encoder_outputs_all[-num_layers:]).mean(dim=[0, 2])
         batch_embeddings = []
